af2a09064c46e9b21fc317c5989f933670fe55ea
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2025-08-06 02:25:48 +0530

    remove unused import

--- lib/screens/extract_apk.dart
+++ lib/screens/extract_apk.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:archive/archive_io.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:installed_apps/app_info.dart';
