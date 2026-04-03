2721435633f652666dfef3ad0723947cf6ce94d1
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2026-04-04 02:13:56 +0530

    web: Refactor adler32 calculation

--- lib/screens/dexrepair/dexrepair_web.dart
+++ lib/screens/dexrepair/dexrepair_web.dart
@@ -64,11 +64,7 @@ class _DexRepairScreenState extends State<DexRepairScreen> {
       // print("Signature: ${sha1Digest}");
     }
 
-    Adler32 adler32 = Adler32();
-    adler32.add(dexData.sublist(12));
-
-    var checksum = adler32.hash;
-    adler32.close();
+    var checksum = getAdler32(dexData.sublist(12));
     // print("Checksum: ${checksum}");
     dexData.buffer.asByteData().setUint32(8, checksum, Endian.little);
 
